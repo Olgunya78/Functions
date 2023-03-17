07b4fd6e445a5195df3f7e57c38221262c1b8413
Olgunya78 <karaulshchikova@mail.ru>
2023-03-17 11:17:51 +0500

    add Seminar_001_1_17

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (х и y), 
+// причем x ≠ 0  и y ≠ 0 и выдает номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if ((x > 0) && (y > 0)) {Console.WriteLine("точка пересечения в 1 четверти плоскости");}
+else if ((x < 0) && (y > 0)) Console.WriteLine("точка пересечения во 2 четверти плоскости");
+else if ((x < 0) && (y < 0)) Console.WriteLine("точка пересечения в 3 четверти плоскости");
+else if ((x > 0) && (y < 0)) Console.WriteLine("точка пересечения в 4 четверти плоскости");
+else Console.WriteLine("x или y равны 0");
