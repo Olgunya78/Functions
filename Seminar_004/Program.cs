7de880cbc5215017ada67d06acbb9f7ad73a14d0
Olgunya78 <karaulshchikova@mail.ru>
2023-03-20 17:34:07 +0500

    add Seminar_004_22

---
+++ Seminar_004/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+void вариант1()
+{
+Console.WriteLine("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+while (i <= n)
+{
+  Console.Write($"{Math.Pow((i),2)}, ");
+  i++;
+}
+}
+void вариант2()
+{
+Console.WriteLine("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+while (i <= n)
+{
+  if (i != n) Console.Write($"{Math.Pow((i),2)}, ");
+  else Console.Write($"{Math.Pow((i),2)}.");
+  i++;
+}
+}
+// вариант1();
+вариант2();
